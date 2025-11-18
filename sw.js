3c8b7885be052ea99d4898c960794d567eeacd7c
MarkRustenholtz <markrusten@gmail.com>
2025-11-18 22:46:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cr-gendarmerie-cache-v11'; // ⚠️ change le numéro à chaque mise à jour
+const CACHE_NAME = 'cr-gendarmerie-cache-v12'; // ⚠️ change le numéro à chaque mise à jour
 const urlsToCache = [
   './',
   './index.html',
