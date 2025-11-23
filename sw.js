17bb623d1d2101261a74ee43b4bb154cae15ee84
MarkRustenholtz <markrusten@gmail.com>
2025-11-23 13:44:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'borderforce-gend-cache-v21'; // ⚠️ change le numéro à chaque mise à jour
+const CACHE_NAME = 'borderforce-gend-cache-v22'; // ⚠️ change le numéro à chaque mise à jour
 const urlsToCache = [
   './',
   './index.html',
